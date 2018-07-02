030f70f9b773efa7987085cbaa9c773fe8454296
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-07-02 17:45:52 +0200

    arch: arm: mpu: move ARMv7m-specific functions in internal header
    This commit refactors arm_mpu.c by moving all internal function
    implementation that is specific to ARMv7-M architecture to a
    separate internal header file. This allows to easily extend
    arm_mpu.c to support ARMv8-M by including all ARMv8-M-specific
    implementation in a similar internal header.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- arch/arm/core/cortex_m/mpu/arm_mpu.c
+++ arch/arm/core/cortex_m/mpu/arm_mpu.c
@@ -14,6 +14,13 @@
 #include <logging/sys_log.h>
 #include <linker/linker-defs.h>
 
+#if defined(CONFIG_CPU_CORTEX_M0PLUS) || \
+	defined(CONFIG_CPU_CORTEX_M3) || \
+	defined(CONFIG_CPU_CORTEX_M4) || \
+	defined(CONFIG_CPU_CORTEX_M7)
+#include <arm_mpu_v7_internal.h>
+#endif
+
 /**
  *  Get the number of supported MPU regions.
  */
@@ -35,85 +42,8 @@ static inline u8_t _get_num_regions(void)
 #endif
 }
 
-/* This internal function performs MPU region initialization.
- *
- * Note:
- *   The caller must provide a valid region index.
- */
-static void _region_init(u32_t index, struct arm_mpu_region *region_conf)
-{
-	/* Select the region you want to access */
-	MPU->RNR = index;
-	/* Configure the region */
-	MPU->RBAR = (region_conf->base & MPU_RBAR_ADDR_Msk)
-				| MPU_RBAR_VALID_Msk | index;
-	MPU->RASR = region_conf->attr | MPU_RASR_ENABLE_Msk;
-	SYS_LOG_DBG("[%d] 0x%08x 0x%08x",
-		index, region_conf->base, region_conf->attr);
-}
-
 #if defined(CONFIG_USERSPACE) || defined(CONFIG_MPU_STACK_GUARD) || \
 	defined(CONFIG_APPLICATION_MEMORY)
-
-/**
- * This internal function converts the region size to
- * the SIZE field value of MPU_RASR.
- */
-static inline u32_t _size_to_mpu_rasr_size(u32_t size)
-{
-	/* The minimal supported region size is 32 bytes */
-	if (size <= 32) {
-		return REGION_32B;
-	}
-
-	/*
-	 * A size value greater than 2^31 could not be handled by
-	 * round_up_to_next_power_of_two() properly. We handle
-	 * it separately here.
-	 */
-	if (size > (1 << 31)) {
-		return REGION_4G;
-	}
-
-	size = 1 << (32 - __builtin_clz(size - 1));
-	return (32 - __builtin_clz(size) - 2) << 1;
-}
-
-/**
- * Generate the value of the MPU Region Attribute and Size Register
- * (MPU_RASR) that corresponds to the supplied MPU region attributes.
- * This function is internal to the driver.
- */
-static inline u32_t _get_region_attr(u32_t xn, u32_t ap, u32_t tex,
-				     u32_t c, u32_t b, u32_t s,
-				     u32_t srd, u32_t region_size)
-{
-	int size = _size_to_mpu_rasr_size(region_size);
-
-	return (((xn << MPU_RASR_XN_Pos) & MPU_RASR_XN_Msk)
-		| ((ap << MPU_RASR_AP_Pos) & MPU_RASR_AP_Msk)
-		| ((tex << MPU_RASR_TEX_Pos) & MPU_RASR_TEX_Msk)
-		| ((s << MPU_RASR_S_Pos) & MPU_RASR_S_Msk)
-		| ((c << MPU_RASR_C_Pos) & MPU_RASR_C_Msk)
-		| ((b << MPU_RASR_B_Pos) & MPU_RASR_B_Msk)
-		| ((srd << MPU_RASR_SRD_Pos) & MPU_RASR_SRD_Msk)
-		| (size));
-}
-
-/**
- * This internal function allocates default cache-ability and share-ability
- * and execution allowance attributes along with the requested access
- * permissions and size.
- */
-static inline arm_mpu_region_attr_t _get_mpu_region_attr(u32_t ap, u32_t base, u32_t size)
-{
-	/* in ARMv7-M MPU the base address is not required
-	 * to determine region attributes
-	 */
-	(void) base;
-
-	return _get_region_attr(1, ap, 0, 1, 0, 1, 0, size);
-}
 
 /**
  * This internal function is utilized by the MPU driver to parse the intent
@@ -139,22 +69,6 @@ static inline arm_mpu_region_attr_t _get_region_attr_by_type(u32_t type,
 		/* Size 0 region */
 		return (arm_mpu_region_attr_t){0};
 	}
-}
-
-/**
- * This internal function is utilized by the MPU driver to combine a given
- * MPU attribute configuration and region size and return the correct
- * parameter set.
- */
-static inline arm_mpu_region_attr_t _get_region_attr_by_conf(u32_t attr,
-	u32_t base, u32_t size)
-{
-	/* in ARMv7-M MPU the base address is not required
-	 * to determine region attributes
-	 */
-	(void) base;
-
-	return attr | _size_to_mpu_rasr_size(size);
 }
 
 /**
@@ -194,57 +108,6 @@ static inline void _disable_region(u32_t r_index)
 	SYS_LOG_DBG("disable region 0x%x", r_index);
 	/* Disable region */
 	ARM_MPU_ClrRegion(r_index);
-}
-
-/**
- * This internal function checks if region is enabled or not.
- *
- * Note:
- *   The caller must provide a valid region number.
- */
-static inline int _is_enabled_region(u32_t r_index)
-{
-	MPU->RNR = r_index;
-
-	return MPU->RASR & MPU_RASR_ENABLE_Msk;
-}
-
-/**
- * This internal function checks if the given buffer is in the region.
- *
- * Note:
- *   The caller must provide a valid region number.
- */
-static inline int _is_in_region(u32_t r_index, u32_t start, u32_t size)
-{
-	u32_t r_addr_start;
-	u32_t r_size_lshift;
-	u32_t r_addr_end;
-
-	MPU->RNR = r_index;
-	r_addr_start = MPU->RBAR & MPU_RBAR_ADDR_Msk;
-	r_size_lshift = ((MPU->RASR & MPU_RASR_SIZE_Msk) >>
-			MPU_RASR_SIZE_Pos) + 1;
-	r_addr_end = r_addr_start + (1 << r_size_lshift) - 1;
-
-	if (start >= r_addr_start && (start + size - 1) <= r_addr_end) {
-		return 1;
-	}
-
-	return 0;
-}
-
-/**
- * This internal function returns the access permissions of an MPU region
- * specified by its region index.
- *
- * Note:
- *   The caller must provide a valid region number.
- */
-static inline u32_t _get_region_ap(u32_t r_index)
-{
-	MPU->RNR = r_index;
-	return MPU->RASR & MPU_RASR_AP_Msk >> MPU_RASR_AP_Pos;
 }
 
 /* ARM Core MPU Driver API Implementation for ARM MPU */
@@ -401,11 +264,6 @@ int arm_core_mpu_get_max_domain_partition_regions(void)
 	return _get_num_regions() -
 		_get_region_index_by_type(THREAD_DOMAIN_PARTITION_REGION);
 }
-
-/* Only a single bit is set for all user accessible permissions.
- * In ARMv7-M MPU this is bit AP[1].
- */
-#define MPU_USER_READ_ACCESSIBLE_Msk (P_RW_U_RO & P_RW_U_RW & P_RO_U_RO & RO)
 
 /**
  * This internal function checks if the region is user accessible or not.
---
+++ arch/arm/core/cortex_m/mpu/arm_mpu_v7_internal.h
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) 2017 Linaro Limited.
+ * Copyright (c) 2018 Nordic Semiconductor ASA.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* This internal function performs MPU region initialization.
+ *
+ * Note:
+ *   The caller must provide a valid region index.
+ */
+static void _region_init(u32_t index, struct arm_mpu_region *region_conf)
+{
+	/* Select the region you want to access */
+	MPU->RNR = index;
+	/* Configure the region */
+	MPU->RBAR = (region_conf->base & MPU_RBAR_ADDR_Msk)
+				| MPU_RBAR_VALID_Msk | index;
+	MPU->RASR = region_conf->attr | MPU_RASR_ENABLE_Msk;
+	SYS_LOG_DBG("[%d] 0x%08x 0x%08x",
+		index, region_conf->base, region_conf->attr);
+}
+
+#if defined(CONFIG_USERSPACE) || defined(CONFIG_MPU_STACK_GUARD) || \
+	defined(CONFIG_APPLICATION_MEMORY)
+
+/**
+ * This internal function converts the region size to
+ * the SIZE field value of MPU_RASR.
+ */
+static inline u32_t _size_to_mpu_rasr_size(u32_t size)
+{
+	/* The minimal supported region size is 32 bytes */
+	if (size <= 32) {
+		return REGION_32B;
+	}
+
+	/*
+	 * A size value greater than 2^31 could not be handled by
+	 * round_up_to_next_power_of_two() properly. We handle
+	 * it separately here.
+	 */
+	if (size > (1 << 31)) {
+		return REGION_4G;
+	}
+
+	size = 1 << (32 - __builtin_clz(size - 1));
+	return (32 - __builtin_clz(size) - 2) << 1;
+}
+
+/**
+ * Generate the value of the MPU Region Attribute and Size Register
+ * (MPU_RASR) that corresponds to the supplied MPU region attributes.
+ * This function is internal to the driver.
+ */
+static inline u32_t _get_region_attr(u32_t xn, u32_t ap, u32_t tex,
+				     u32_t c, u32_t b, u32_t s,
+				     u32_t srd, u32_t region_size)
+{
+	int size = _size_to_mpu_rasr_size(region_size);
+
+	return (((xn << MPU_RASR_XN_Pos) & MPU_RASR_XN_Msk)
+		| ((ap << MPU_RASR_AP_Pos) & MPU_RASR_AP_Msk)
+		| ((tex << MPU_RASR_TEX_Pos) & MPU_RASR_TEX_Msk)
+		| ((s << MPU_RASR_S_Pos) & MPU_RASR_S_Msk)
+		| ((c << MPU_RASR_C_Pos) & MPU_RASR_C_Msk)
+		| ((b << MPU_RASR_B_Pos) & MPU_RASR_B_Msk)
+		| ((srd << MPU_RASR_SRD_Pos) & MPU_RASR_SRD_Msk)
+		| (size));
+}
+
+/**
+ * This internal function allocates default cache-ability and share-ability
+ * and execution allowance attributes along with the requested access
+ * permissions and size.
+ */
+static inline arm_mpu_region_attr_t _get_mpu_region_attr(u32_t ap, u32_t base, u32_t size)
+{
+	/* in ARMv7-M MPU the base address is not required
+	 * to determine region attributes
+	 */
+	(void) base;
+
+	return _get_region_attr(1, ap, 0, 1, 0, 1, 0, size);
+}
+
+/**
+ * This internal function is utilized by the MPU driver to combine a given
+ * MPU attribute configuration and region size and return the correct
+ * parameter set.
+ */
+static inline arm_mpu_region_attr_t _get_region_attr_by_conf(u32_t attr,
+	u32_t base, u32_t size)
+{
+	/* in ARMv7-M MPU the base address is not required
+	 * to determine region attributes
+	 */
+	(void) base;
+
+	return attr | _size_to_mpu_rasr_size(size);
+}
+
+/**
+ * This internal function checks if region is enabled or not.
+ *
+ * Note:
+ *   The caller must provide a valid region number.
+ */
+static inline int _is_enabled_region(u32_t r_index)
+{
+	MPU->RNR = r_index;
+
+	return MPU->RASR & MPU_RASR_ENABLE_Msk;
+}
+
+/**
+ * This internal function checks if the given buffer is in the region.
+ *
+ * Note:
+ *   The caller must provide a valid region number.
+ */
+static inline int _is_in_region(u32_t r_index, u32_t start, u32_t size)
+{
+	u32_t r_addr_start;
+	u32_t r_size_lshift;
+	u32_t r_addr_end;
+
+	MPU->RNR = r_index;
+	r_addr_start = MPU->RBAR & MPU_RBAR_ADDR_Msk;
+	r_size_lshift = ((MPU->RASR & MPU_RASR_SIZE_Msk) >>
+			MPU_RASR_SIZE_Pos) + 1;
+	r_addr_end = r_addr_start + (1 << r_size_lshift) - 1;
+
+	if (start >= r_addr_start && (start + size - 1) <= r_addr_end) {
+		return 1;
+	}
+
+	return 0;
+}
+
+/**
+ * This internal function returns the access permissions of an MPU region
+ * specified by its region index.
+ *
+ * Note:
+ *   The caller must provide a valid region number.
+ */
+static inline u32_t _get_region_ap(u32_t r_index)
+{
+	MPU->RNR = r_index;
+	return MPU->RASR & MPU_RASR_AP_Msk >> MPU_RASR_AP_Pos;
+}
+
+/* Only a single bit is set for all user accessible permissions.
+ * In ARMv7-M MPU this is bit AP[1].
+ */
+#define MPU_USER_READ_ACCESSIBLE_Msk (P_RW_U_RO & P_RW_U_RW & P_RO_U_RO & RO)
+
+#endif /* USERSPACE || MPU_STACK_GUARD || APPLICATION_MEMORY */
