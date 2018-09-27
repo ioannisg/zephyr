005ea116d28b5e12554c4582e260a2bdd4c74271
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-09-27 16:15:15 +0200

    arch: arm: function to (re)initialize MPU region for kernel ram
    This commit introduces a function to reset the MPU region for
    the whole kernel ram to its initial configuration. In addition,
    it disables any MPU regions in kernel ram space. The function is
    to be used upon context switch, for MPU implementations that
    require non-overlapping regions. It is needed only if the kernel
    has features (e.g. Thread Stacks or Stack Guards) that generate
    MPU regions (partitions) inside the kernel SRAM area. For
    consistency, the access permissions of the kernel ram area are
    set, also, during initialization.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- arch/arm/core/cortex_m/mpu/arm_core_mpu.c
+++ arch/arm/core/cortex_m/mpu/arm_core_mpu.c
@@ -12,6 +12,20 @@
 #include <arch/arm/cortex_m/mpu/arm_core_mpu.h>
 #include <logging/sys_log.h>
 
+#if defined(CONFIG_MPU_REQUIRES_NON_OVERLAPPING_REGIONS)
+#if defined(CONFIG_MPU_STACK_GUARD) || defined(CONFIG_USERSPACE)
+/*
+ * @brief Reset Kernel SRAM region access permissions at context-switch.
+ */
+void configure_mpu_kernel_ram_region_reset(void)
+{
+	arm_core_mpu_disable();
+	arm_core_mpu_kernel_ram_region_reset();
+	arm_core_mpu_enable();
+}
+#endif /* CONFIG_MPU_STACK_GUARD || CONFIG_USERSPACE */
+#endif
+
 #if defined(CONFIG_MPU_STACK_GUARD)
 /*
  * @brief Configure MPU stack guard
