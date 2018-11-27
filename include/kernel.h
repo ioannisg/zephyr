28d90a8c38ef975ddeb8533a9795ce2522542ade
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-11-27 15:45:36 +0100

    kernel: expose k_mem_partition_attr_t outside User mode
    This commit exposes k_mem_partition_attr_t outside the
    User mode, so we can use k_mem_partition for defining
    memory partitions outside the scope of user mode (for
    example, supervisor thread stack guards). In addition,
    the commit exposes the MPU variant-specific attribute
    macros outside the User mode context, so they can be
    used in a more generic way.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- include/kernel.h
+++ include/kernel.h
@@ -4732,10 +4732,8 @@ struct k_mem_partition {
 	u32_t start;
 	/* size of memory partition */
 	u32_t size;
-#ifdef CONFIG_USERSPACE
 	/* attribute of memory partition */
 	k_mem_partition_attr_t attr;
-#endif	/* CONFIG_USERSPACE */
 };
 
 /* memory domain
