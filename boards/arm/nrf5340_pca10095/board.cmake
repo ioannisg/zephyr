be899945a080ca43e2b1803e17ed4df481060373
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2019-08-02 23:53:01 +0200

    boards: arm: nordic: nRF5340_pca10095_cpu0 board definition
    - Board cmake file with runner commands for Application MCU
    - nRF5340 PCA10095 CPU0 board definition (Application MCU)
      Secure and Non-Secure board definition
    - nrf5340 shared SRAM partitioning info
    - nrfjprog runner script updates for nRF5340
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

---
+++ boards/arm/nrf5340_pca10095/board.cmake
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: Apache-2.0
+
+if(CONFIG_BOARD_NRF5340_PCA10095_CPU0 OR CONFIG_BOARD_NRF5340_PCA10095_CPU0NS)
+board_runner_args(nrfjprog "--nrf-family=NRF53"-tool-opt=--coprocessor CP_APPLICATION")
+board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
+endif()
+
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
