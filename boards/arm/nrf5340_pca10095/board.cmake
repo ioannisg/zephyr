33261f49f839e44b9bc3d32ecd19f2aa61a05d3f
Gaute Gamnes <gaute.gamnes@nordicsemi.no>
2019-08-09 16:20:44 +0200

    boards: arm: nordic: nRF5340_pca10095_cpu1 board definition
    - Modify board cmake file for Network MCU
    - nRF5340 PCA10095 CPU1 board definition (Network MCU)
    
    Signed-off-by: Gaute Gamnes <gaute.gamnes@nordicsemi.no>

--- boards/arm/nrf5340_pca10095/board.cmake
+++ boards/arm/nrf5340_pca10095/board.cmake
@@ -5,5 +5,9 @@ board_runner_args(nrfjprog "--nrf-family=NRF53"-tool-opt=--coprocessor CP_APPLIC
 board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
 endif()
 
+if(CONFIG_BOARD_NRF5340_PCA10095_CPU1)
+board_runner_args(nrfjprog "--nrf-family=NRF53" "--tool-opt=--coprocessor CP_NETWORK")
+endif()
+
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
